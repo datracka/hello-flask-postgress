351dc103d29ea54cb7fbda40d3adbb969a56acc3
Vicens Fayos <datracka@gmail.com>
2019-10-24 12:50:59 +0200

    addding support some support configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# hello World Flask Postgress Project
+
+## to run it 
+- `"pipenv install"`
+- `"python run.py"`
+- enjoy!
--- app/config.py
+++ app/config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS=1
+    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
